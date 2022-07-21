b0a2591deb2cb96f517aa7bc5c3d275cfd0e4450
lazoapparently <lazarusmamai@gmail.com>
2022-07-21 08:14:05 +0000

    Added a function that prints an integer with {:d}.format().

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,6 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(item), end="")
                 print("")
                 return lstlen
-
             except IndexError:
                 if x > lstlen:
                     return
