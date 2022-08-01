27d851f29ce6b7fb9e7bfafa5d4a75f340b283e7
lazoapparently <lazarusmamai@gmail.com>
2022-08-01 18:48:27 +0000

    Added rectangle file

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 01 2022
+@author: Lazo Apparently
+"""
+
+
 class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+    empty
+    """
     pass
