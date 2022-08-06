596eae737d553c1d8ef3ba1b4a80bdc26b76b0fb
lazoapparently <lazarusmamai@gmail.com>
2022-08-06 10:16:08 +0000

    Fixed loop error in task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,10 +20,9 @@ class Rectangle:
         """setter for private instance width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -35,7 +34,6 @@ class Rectangle:
         """setter for the private attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
