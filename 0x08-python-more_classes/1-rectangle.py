5ca2f597d43f4d34f7deda9d7470504d4691c746
lazoapparently <lazarusmamai@gmail.com>
2022-08-06 08:27:36 +0000

    Fixed comments on tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,41 +4,20 @@ Defines a class rectangle.
 """
 
 class Rectangle:
-    """
-    Define a rectangle object class
-    """
+    """Define a rectangle object class"""
     def __init__(self, width=0, height=0):
-    """
-    Args:
-    width (int): Default width for the class object.
-    height (int): Default height for the class object.
-    """
+    """Initializes a rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-    """
-    Setter for the private attribute width
-    Args:
-    None
-    Return:
-    The return. The private attribute width
-    """
+    """The private attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        sETTER WIDTH OF THE RECTANGLE
-
-        Attributes:
-            width (int): The width of the rectagle
-
-        Raises:
-            TypeError: If width is not an integer
-            ValueError: If width is less than 0
-        """
+        """setter for private instance width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -48,26 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Property height to retrieve it
-
-        Returns:
-            height (int): The height of the rectangle
-        """
+        """getter for private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Setter height of the rectangle
-
-        Attributes:
-            height (int): The height of the rectangle
-
-        Raises:
-            TypeError: If height i s not an integer
-            ValueError: If height is less than 0
-        """
+        """setter for the private attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
