df99dfd00b2f5b9142674d8ca9bc7dfa75dadde3
lazoapparently <lazarusmamai@gmail.com>
2022-08-15 16:02:35 +0000

    Added a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates a class object"""
+
+
+class Square:
+    """Defines a Square"""
+    def __init__(self, size):
+        """Creates a private instance attribute"""
+        self.__size = size
