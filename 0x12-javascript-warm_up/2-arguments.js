3af17ab5c57b43ccb06724df2adb989f53f19cf0
lazoapparently <lazarusmamai@gmail.com>
2022-10-04 16:11:49 +0000

    Fixed substandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,16 +2,15 @@
 
 const argNum = process.argv.length;
 
-switch (argNum)
-{
-	case 2:
-		console.log('No argument');
-		break;
+switch (argNum) {
+  case 2:
+    console.log('No argument');
+    break;
 
-	case 3:
-		console.log('Argument found')
-		break;
+  case 3:
+    console.log('Argument found');
+    break;
 
-	default:
-		console.log('Arguments found');
+  default:
+    console.log('Arguments found');
 }
