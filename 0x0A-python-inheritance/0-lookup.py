cabcd51c55edb9a309b258c810e160881213b12a
lazoapparently <lazarusmamai@gmail.com>
2022-11-12 00:25:32 +0000

    Added a class MyList that inherits from list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-#A function that defines an object
-
+# A function that defines an object
 """Defines an object attribute lookup function."""
 
 
 def lookup(obj):
 	"""Return a list of an object's available attributes."""
-	return (dir(obj))
+return (dir(obj))
