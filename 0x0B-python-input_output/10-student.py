7def6094965f1f568b49690aa0d65ad08d3c5363
lazoapparently <lazarusmamai@gmail.com>
2022-11-17 11:46:30 +0000

    Added a class Student that defines a student PI

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,13 +15,13 @@ class Student:
 
     def to_json(self, attrs=None):
         """Retrieves a dictionary of this students attributes.
-         Returns:
-             dict: A dictionary of this students attr
+        Returns:
+            dict: A dictionary of this students attr
         """
         if '__dict__' in dir(self):
             res = dict()
             can_filter = False
-            if (type(attrs) is list) and all(types() is str for s in attrs):
+            if (type(attrs) is list) and all(type(s) is str for s in attrs):
                 can_filter = True
             for key in self.__dict__.keys():
                 if (not can_filter) or (can_filter and key in attrs):
