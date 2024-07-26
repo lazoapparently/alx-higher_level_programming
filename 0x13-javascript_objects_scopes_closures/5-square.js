f23e339c76ba9ea369035e4dcc9a23e31afe1a83
Lazo Apparently <lazarusmamai@gmail.com>
2024-07-26 16:17:14 +0000

    a class defining a square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,3 +1,10 @@
-#1/usr/bin/node
+#!/usr/bin/node
 
-class Rectangle
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
+	constructor (size) {
+		super(size, size);
+		this.size = size;
+	}
+};
