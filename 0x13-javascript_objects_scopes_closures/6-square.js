e05e88db485289f6aa900c104961a2b6e0336b2d
Lazo Apparently <lazarusmamai@gmail.com>
2024-07-26 16:28:09 +0000

    Python is fun

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const sq = require('./5-square.js');
+
+module.exports = class Square extends sq {
+	charPrint (c) {
+		if (c === undefined) {
+			this.print();
+		} else {
+			for (let i = 0; i < this.width; i++) {
+				console.log(c.repeat(this.height));
+			}
+		}
+	}
+};
